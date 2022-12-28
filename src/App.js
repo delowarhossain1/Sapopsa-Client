19341e8cf52fec6d90af369288c5211d45e56664
DeveloperDelowar <programmerdaloar@gmail.com>
2022-12-28 09:09:15 -0800

    home page updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Home from './components/pages/Home/Home';
 import Footer from './components/shared/Footer/Footer';
-import Navbar from './components/shared/Navbar/Navbar';
 import "./css/about.css";
 import "./css/buy-now.css";
 import "./css/contact.css";
