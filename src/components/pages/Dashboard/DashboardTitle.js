bcbaa28d9654a95b1d324de4033f247b4888845e
DeveloperDelowar <programmerdaloar@gmail.com>
2022-12-30 11:34:49 -0800

    dashboard title component updated

--- src/components/pages/Dashboard/DashboardTitle.js
+++ src/components/pages/Dashboard/DashboardTitle.js
@@ -1,13 +1,32 @@
 import React from 'react';  
+import { FaHome } from 'react-icons/fa';
+import { AiOutlineLogout } from 'react-icons/ai';
 import css from "../../../css/DashboardTitle.module.css";
+import { Link } from 'react-router-dom';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import Loading from '../../shared/Loading/Loading';
 
 const DashboardTitle = () => {
+    const [user, userLoading] = useAuthState(auth);
+    const [signOut, signOutLoading] = useSignOut(auth);
+    
+    if(userLoading && signOutLoading){
+        return <Loading />
+    }
+
     return (
-        <div>
+        <div className={css.head}>
             <h2 className={css.dashboardTitle}>Dashboard</h2>
 
-            <div>
-                
+            <div className={css.icon}>
+                <Link to='/' title='Home page' className={css.homeIcon}><FaHome /></Link>
+
+                <span title='Log out' className={css.logOut} onClick={()=> signOut()}><AiOutlineLogout /></span>
+
+                <span className={css.profile}>
+                    <img src={user.photoURL} alt='Profile' />
+                </span>
             </div>
         </div>
     );
