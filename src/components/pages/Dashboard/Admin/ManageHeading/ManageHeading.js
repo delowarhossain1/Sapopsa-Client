0086e7cd7cf255411922eea4ae091e1aaadf24ba
DeveloperDelowar <programmerdaloar@gmail.com>
2023-01-03 11:59:09 -0800

    updated

--- src/components/pages/Dashboard/Admin/ManageHeading/ManageHeading.js
+++ src/components/pages/Dashboard/Admin/ManageHeading/ManageHeading.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DashboardTitle from './../../DashboardTitle';
 import css from "../../../../../css/ManageHeading.module.css";
+import PageTitle from '../../../../shared/PageTitle/PageTitle';
 
 const ManageHeading = () => {
     return (
         <div>
             <DashboardTitle title='Heading' />
+            <PageTitle title='manage heading' />
 
             <div>
                 <div style={{background: '#ddd', padding : '4px'}}>In publishing and graphic design, Lorem ipsum is a placeholder text commonly</div>
