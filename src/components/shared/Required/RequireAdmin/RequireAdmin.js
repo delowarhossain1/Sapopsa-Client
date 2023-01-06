ae13f759ee17d86163c72b1588d018e2e9d0dd41
DeveloperDelowar <programmerdaloar@gmail.com>
2023-01-06 12:36:20 -0800

    requireAdmin

--- src/components/shared/Required/RequireAdmin/RequireAdmin.js
+++ src/components/shared/Required/RequireAdmin/RequireAdmin.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Loading from '../../Loading/Loading';
 import auth from './../../../../firebase.init';
+import useAdmin from './../../../../hooks/useAdmin';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-const RequireAdmin = () => {
+const RequireAdmin = ({children}) => {
+    const navigate = useNavigate();
     const [user, loading] = useAuthState(auth);
-    
-    return (
-        <div>
-            
-        </div>
-    );
+    const [isAdmin, adminLoading] = useAdmin(user);
+
+    if(loading || adminLoading){
+        return <Loading />
+    }
+
+    if(! isAdmin){
+        signOut(auth);
+        navigate('/login');
+        return;
+    }
+
+    return children;
 };
 
 export default RequireAdmin;
--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -1,16 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-const useAdmin = () => {
+const useAdmin = (user) => {
     const [admin, setAdmin] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
-        fetch('')
-        .then(res => res.json())
-        .then(res => setAdmin(res));
-    }, []);
+        if(user){
+            const email = user?.email;
+
+            fetch(`http://localhost:5000/is-admin/${email}`)
+            .then(res => res.json())
+            .then(adminInfo =>{
+                setAdmin(adminInfo?.isAdmin);
+                setLoading(false);
+            })
+        }
+    }, [user]);
 
 
-    return 
+    return [admin, loading];
 };
 
 export default useAdmin;
