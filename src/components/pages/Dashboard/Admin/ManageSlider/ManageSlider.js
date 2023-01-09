0d74be3671852aedab0bb4eaf5aafa43d4315903
DeveloperDelowar <programmerdaloar@gmail.com>
2023-01-09 11:16:52 -0800

    manage slider updated

--- src/components/pages/Dashboard/Admin/ManageSlider/ManageSlider.js
+++ src/components/pages/Dashboard/Admin/ManageSlider/ManageSlider.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboardTitle from '../../DashboardTitle';
 import PageTitle from '../../../../shared/PageTitle/PageTitle';
 import css from "../../../../../css/ManageSlider.module.css";
 import css2 from "../../../../../css/Table.module.css";
 import { Link } from 'react-router-dom';
-import img1 from "../../../../../images/1.jpg";
+import useModal from './../../../../../hooks/useModal';
 
 const ManageSlider = () => {
+    const {deleteModal} = useModal();
+    const [sliders, setSliders] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/sliders',)
+            .then(res => res.json())
+            .then(res => setSliders(res));
+    }, []);
+
     return (
         <div>
             <DashboardTitle title='Sliders' />
@@ -27,16 +36,23 @@ const ManageSlider = () => {
                         <th>Action</th>
                     </tr>
 
-                    <tr>
-                        <th>01</th>
-                        <th>
-                            <img src={img1} className={css2.categoryImg} alt="" />
-                        </th>
-                        <th>This is a title.</th>
-                        <th>
-                            <button className={css2.deleteBtn}>Delete</button>
-                        </th>
-                    </tr>
+                    {
+                        sliders?.map((slider, i) => (
+                            <tr key={slider?._id}>
+                                <th>{i + 1}</th>
+                                <th>
+                                    <img src={slider?.img} className={css2.categoryImg} alt="" />
+                                </th>
+                                <th>{slider?.title}</th>
+                                <th>
+                                    <button 
+                                        className={css2.deleteBtn}
+                                        onClick={()=> deleteModal()}
+                                    >Delete</button>
+                                </th>
+                            </tr>
+                        ))
+                    }
                 </table>
             </div>
         </div>
