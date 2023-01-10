485c1da97707788cf7bbf6472a6e6ea8c7f74523
DeveloperDelowar <programmerdaloar@gmail.com>
2023-01-10 12:01:38 -0800

    customers

--- src/components/pages/Dashboard/Admin/Customers/Customers.js
+++ src/components/pages/Dashboard/Admin/Customers/Customers.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageTitle from '../../../../shared/PageTitle/PageTitle';
 import DashboardTitle from '../../DashboardTitle';
 import css from "../../../../../css/Table.module.css";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../../../firebase.init';
+import { getAccessToken } from '../../../../../utilites/setAndGetAccessToken';
+import Loading from '../../../../shared/Loading/Loading';
 
 const Customers = () => {
+    const [user, loading] = useAuthState(auth);
+    const [allUsers, setAllUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/customers?email=${user?.email}`, {
+            headers : {
+                auth : `Bearer ${getAccessToken()}`
+            }
+        })
+            .then(res => res.json())
+            .then(res => setAllUsers(res));
+
+    }, [user]);
+
+
+    if(loading){
+        return <Loading />
+    }
+
     return (
         <div>
             <DashboardTitle title='Customers' />
@@ -18,14 +41,21 @@ const Customers = () => {
                         <th>Action</th>
                     </tr>
 
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th>
-                            <button className={css.btn}>Make Admin</button>
-                        </th>
-                    </tr>
+                    {
+                        allUsers?.map((u, i) => (
+                            <tr key={u?._id}>
+                                <th>{i + 1}</th>
+                                <th>{u?.name}</th>
+                                <th>{u?.email}</th>
+                                <th>
+                                    <button 
+                                        className={css.btn}
+                                        disabled= {u?.role === 'admin'}
+                                        >Make Admin</button>
+                                </th>
+                            </tr>
+                        ))
+                    }
                 </table>
             </div>
         </div>
--- src/components/pages/Dashboard/Admin/ManageHeading/ManageHeading.js
+++ src/components/pages/Dashboard/Admin/ManageHeading/ManageHeading.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboardTitle from './../../DashboardTitle';
 import css from "../../../../../css/ManageHeading.module.css";
 import PageTitle from '../../../../shared/PageTitle/PageTitle';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../../../firebase.init';
+import Loading from '../../../../shared/Loading/Loading';
+import { getAccessToken } from "../../../../../utilites/setAndGetAccessToken";
 
 const ManageHeading = () => {
+    const [user, loading] = useAuthState(auth);
+    const [heading, setHeading] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/web-heading')
+            .then(res => res.json())
+            .then(res => setHeading(res));
+
+    }, []);
+
+    // handle update heading
+    const handleHading = (e) => {
+        e.preventDefault();
+        const heading = e.target.heading.value;
+
+        if (heading && user) {
+            
+            fetch(`http://localhost:5000/web-heading?email=${user?.email}`, {
+                method: 'PUT',
+                headers: {
+                    "content-type": "application/json",
+                    auth: getAccessToken()
+                },
+                body: JSON.stringify(heading)
+            })
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res)
+                    if (res) {
+
+                    }
+                })
+        }
+    }
+
+    if (loading) {
+        return <Loading />
+    }
+
     return (
         <div>
             <DashboardTitle title='Heading' />
             <PageTitle title='manage heading' />
 
             <div>
-                <div style={{background: '#ddd', padding : '4px'}}>In publishing and graphic design, Lorem ipsum is a placeholder text commonly</div>
+                {
+                    heading?.map(h => (
+                        <div key={h?._id} style={{ background: '#ddd', padding: '4px' }}>{h.heading}</div>
+                    ))
+                }
 
-                <form className={css.form}>
-                    <textarea placeholder='Update Heading'></textarea>
+                <form className={css.form} onSubmit={handleHading}>
+                    <textarea placeholder='Update Heading' name='heading'></textarea>
                     <button type="submit">Update</button>
                 </form>
             </div>
