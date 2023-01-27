d149f8e511ad774353284e42d7d76529ff2a3a80
Developer Delowar <daloar2400@gmail.com>
2023-01-26 22:28:14 -0800

    add new slider component updated

--- src/components/pages/Dashboard/Admin/ManageSlider/AddNewSlider.js
+++ src/components/pages/Dashboard/Admin/ManageSlider/AddNewSlider.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DashboardTitle from '../../DashboardTitle';
 import css from "../../../../../css/AddNewCategory.module.css";
+import axios from 'axios';
+import { getAccessToken } from "../../../../../utilites/setAndGetAccessToken";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../../../../firebase.init';
+import Loading from '../../../../shared/Loading/Loading';
+import useModal from './../../../../../hooks/useModal';
+import { useNavigate } from 'react-router-dom';
 
 const AddNewSlider = () => {
+    const [user, loading] = useAuthState(auth);
+    const [sliderImage, setSliderImage] = useState(null);
+    const { successFullModal, simpleMessageDisplay } = useModal();
+    const navigate = useNavigate();
 
-    const handleSubmit = () =>{
-        fetch('http://localhost:5000/sliders', {
-            method : "PUT",
-            headers : {
-                "Content-Type" : "application/json"
-            },
-            body : JSON.stringify({name : "delowar"})
-        })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const type = /image.*/;
+
+        if (sliderImage.type.match(type)) {
+            const title = e.target.title.value;
+            const formData = new FormData();
+            formData.append('img', sliderImage);
+            formData.append('title', title);
+
+            const url = `http://localhost:5000/sliders?email=${user?.email}`;
+            axios.post(url, formData, {
+                headers: {
+                    auth: `Bearer ${getAccessToken()}`
+                },
+            })
+                .then(res => {
+                    if (res?.data?.insertedId && res?.status === 200) {
+                        successFullModal();
+                        navigate('/dashboard/manage-slider/')
+                    }
+                    else {
+                        simpleMessageDisplay('Slider upload failed. Please try again.')
+                    }
+                });
+        }
+        else{
+            simpleMessageDisplay("Unable to upload this file. Try an image file.");
+        }
+
+    }
+
+    if (loading) {
+        return <Loading />
     }
 
     return (
@@ -21,8 +59,8 @@ const AddNewSlider = () => {
             <div className={css.newCategoryFrom}>
                 <form className={css.form} onSubmit={handleSubmit}>
                     <h4>Add new slider</h4>
-                    <input type="text" placeholder='Slider Title' name="" />
-                    <input type='file' name="" />
+                    <input type="text" placeholder='Slider Title' name="title" required />
+                    <input type='file' onChange={(e) => setSliderImage(e.target.files[0])} required />
                     <button type='submit'>Submit</button>
                 </form>
             </div>
