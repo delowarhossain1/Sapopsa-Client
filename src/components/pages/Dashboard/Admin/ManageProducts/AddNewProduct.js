af20a46058e68b64daaf779d9cbc97ca672c5f90
Developer Delowar <daloar2400@gmail.com>
2023-05-10 11:32:54 -0700

    Add new product component updated

--- src/components/pages/Dashboard/Admin/ManageProducts/AddNewProduct.js
+++ src/components/pages/Dashboard/Admin/ManageProducts/AddNewProduct.js
@@ -15,6 +15,10 @@ const AddNewProduct = () => {
     const clrRef = useRef();
     const speciRef = useRef();
     const sizeRef = useRef();
+    const sizeChartLaval1 = useRef();
+    const sizeChartLaval2 = useRef();
+    const sizeChartLaval3 = useRef();
+    const sizeChartLaval4 = useRef();
     const navigate = useNavigate();
     const [productUploading, setProductUploading] = useState(false);
     const [user, loading] = useAuthState(auth);
@@ -23,9 +27,10 @@ const AddNewProduct = () => {
     const [specification, setSpecification] = useState([]);
     const [allSize, setAllSize] = useState([]);
     const [categories, setCategories] = useState({});
+    const [sizeChartData, setSizeChatData] = useState([]);
 
     // Get all categories
-    useEffect(()=>{
+    useEffect(() => {
         axios.get('/api/categories-list')
             .then(res => setCategories(res?.data));
     }, [])
@@ -78,6 +83,22 @@ const AddNewProduct = () => {
         setAllSize(rest);
     }
 
+    // Handle size chart
+    const handleSizeChart = () => {
+        const label1 = sizeChartLaval1.current.value || "Title";
+        const label2 = sizeChartLaval2.current.value || "Title";
+        const label3 = sizeChartLaval3.current.value || "Title";
+        const label4 = sizeChartLaval4.current.value || "Title";
+
+        setSizeChatData([...sizeChartData, { label1, label2, label3, label4 }]);
+
+        // Empty input field
+        sizeChartLaval1.current.value = '';
+        sizeChartLaval2.current.value = '';
+        sizeChartLaval3.current.value = '';
+        sizeChartLaval4.current.value = '';
+    }
+
     // Handle add product
     const handleProduct = (event) => {
         // Set loading status;
@@ -101,6 +122,7 @@ const AddNewProduct = () => {
         if (displayIMG?.type?.match(reg) && galleryIMG?.length < 3) {
             const title = e.title.value;
             const price = e.price.value;
+            const regularPrice = e.regularPrice.value;
             const thisIsFor = e.thisIsFor.value;
             const category = e.category.value;
             const des = e.des.value;
@@ -108,6 +130,7 @@ const AddNewProduct = () => {
             const formData = new FormData();
             formData.append('title', title);
             formData.append('price', price);
+            formData.append('regularPrice', regularPrice);
             formData.append('thisIsFor', thisIsFor);
             formData.append('category', category);
             formData.append('des', des);
@@ -118,29 +141,30 @@ const AddNewProduct = () => {
             formData.append('size', JSON.stringify(allSize));
             formData.append('specification', JSON.stringify(specification));
             formData.append('colors', JSON.stringify(productCLR));
+            formData.append('sizeChart', JSON.stringify(sizeChartData));
 
             // Update database
             const url = `/api/product?email=${user?.email}`;
             axios.post(url, formData, {
-                headers : {
-                    auth : `Bearer ${getAccessToken()}`
+                headers: {
+                    auth: `Bearer ${getAccessToken()}`
                 }
             })
-            .then(res => {
-                if(res?.data?.insertedId && res?.status === 200){
-                     // Set loading status;
-                    setProductUploading(false);
+                .then(res => {
+                    if (res?.data?.insertedId && res?.status === 200) {
+                        // Set loading status;
+                        setProductUploading(false);
 
-                    successFullModal();
-                    navigate('/dashboard/manage-products');
-                }
-                else{
-                    // Set loading status;
-                    setProductUploading(false);
-                    // Error message
-                    simpleMessageDisplay("Please try again.");
-                }
-            });
+                        successFullModal();
+                        navigate('/dashboard/manage-products');
+                    }
+                    else {
+                        // Set loading status;
+                        setProductUploading(false);
+                        // Error message
+                        simpleMessageDisplay("Please try again.");
+                    }
+                });
         }
         else {
             // Set loading status;   
@@ -161,25 +185,25 @@ const AddNewProduct = () => {
 
             <form onSubmit={handleProduct}>
                 <div className={css.row1}>
-                    <label for="title">Title</label>
-                    <input type='text' id='title' name='title' placeholder='Product title' required/>
+                    <label htmlFor="title">Title</label>
+                    <input type='text' id='title' name='title' placeholder='Product title' required />
                 </div>
 
                 <div className={css.row2}>
                     {/* product size */}
                     <div>
                         <div className={css.sizeBtn}>
-                            <label for="size">
+                            <label htmlFor="size">
                                 <span>Size :</span>
 
                                 {
-                                    allSize?.map((btn, i)=> (
-                                        <button 
+                                    allSize?.map((btn, i) => (
+                                        <button
                                             title='click to remove'
-                                             type="button"
-                                             onClick={()=> removeSize(i)}
-                                             key={i}
-                                            >{btn}</button>
+                                            type="button"
+                                            onClick={() => removeSize(i)}
+                                            key={i}
+                                        >{btn}</button>
                                     ))
                                 }
 
@@ -191,14 +215,21 @@ const AddNewProduct = () => {
                         </div>
                     </div>
 
-                    <div>
-                        <label for="price">Price</label>
-                        <input type='number' name='price' id='price' placeholder='$100' min='1' required />
+                    <div className={css.priceBox}>
+                        <div>
+                            <label htmlFor="regularPrice">Regular Price</label>
+                            <input type='number' name='regularPrice' id='regularPrice' placeholder='$100' min='1' required autoComplete='off' />
+                        </div>
+
+                        <div>
+                            <label htmlFor="price">Price</label>
+                            <input type='number' name='price' id='price' placeholder='$100' min='1' required autoComplete='off' />
+                        </div>
                     </div>
 
 
                     <div>
-                        <label for="for">For</label>
+                        <label htmlFor="for">For</label>
                         <select id='for' name='thisIsFor' required>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
@@ -207,54 +238,54 @@ const AddNewProduct = () => {
                     </div>
 
                     <div>
-                        <label for="category">Category</label>
+                        <label htmlFor="category">Category</label>
                         <select id='category' name='category' required>
                             {
                                 categories?.men?.map(c => (
-                                    <option 
+                                    <option
                                         key={c?._id}
                                         value={c?.route}>
-                                            {c?.title}
+                                        {c?.title}
                                     </option>
                                 ))
                             }
                             {
                                 categories?.women?.map(c => (
-                                    <option 
+                                    <option
                                         key={c?._id}
                                         value={c?.route}>
-                                            {c?.title}
+                                        {c?.title}
                                     </option>
                                 ))
                             }
                             {
                                 categories?.sports?.map(c => (
-                                    <option 
+                                    <option
                                         key={c?._id}
                                         value={c?.route}>
-                                            {c?.title}
+                                        {c?.title}
                                     </option>
                                 ))
                             }
-                            
+
                         </select>
                     </div>
                 </div>
 
                 <div className={css.row4}>
                     <div>
-                        <label for="displayIMG">Display IMG</label>
-                        <input type="file" id='displayIMG' name="displayIMG" required/>
+                        <label htmlFor="displayIMG">Display IMG</label>
+                        <input type="file" id='displayIMG' name="displayIMG" required />
                     </div>
 
                     <div>
-                        <label for="galleryIMG">Gallery IMG</label>
-                        <input type="file" id='galleryIMG' name="galleryIMG" multiple required/>
+                        <label htmlFor="galleryIMG">Gallery IMG</label>
+                        <input type="file" id='galleryIMG' name="galleryIMG" multiple required />
                     </div>
 
                     <div>
                         <div className={css.colorBox}>
-                            <label for="colorPic">Color : </label>
+                            <label htmlFor="colorPic">Color : </label>
                             <div>
                                 {
                                     productCLR?.map((clr, i) => (
@@ -280,7 +311,7 @@ const AddNewProduct = () => {
 
                 <div className={css.row3}>
                     <div>
-                        <label for="des">Description</label>
+                        <label htmlFor="des">Description</label>
                         <textarea name='des' id='des' placeholder='Description' required></textarea>
                     </div>
 
@@ -305,6 +336,40 @@ const AddNewProduct = () => {
                             <input type="text" placeholder='Specification' ref={speciRef} />
                             <button type="button" onClick={handleSpecification}>Add</button>
                         </div>
+                    </div>
+                </div>
+
+
+                {/* Size chart */}
+                <div className={css.sizeChart}>
+                    <div>
+                        <h4>Size chart</h4>
+                        <div>
+                            <input type="text" placeholder='Label 1' ref={sizeChartLaval1} />
+                            <input type="text" placeholder='Label 2' ref={sizeChartLaval2} />
+                        </div>
+                        <div>
+                            <input type="text" placeholder='Label 3' ref={sizeChartLaval3} />
+                            <input type="text" placeholder='Label 4' ref={sizeChartLaval4} />
+                        </div>
+
+                        <button type='button' onClick={handleSizeChart}>Add</button>
+                    </div>
+
+                    {/* Size chart display */}
+                    <div>
+                        <table>
+                            {
+                                sizeChartData?.map((list, index) => (
+                                    <tr key={index * Math.random()}>
+                                        <td>{list?.label1}</td>
+                                        <td>{list?.label2}</td>
+                                        <td>{list?.label3}</td>
+                                        <td>{list?.label4}</td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
                     </div>
                 </div>
 
